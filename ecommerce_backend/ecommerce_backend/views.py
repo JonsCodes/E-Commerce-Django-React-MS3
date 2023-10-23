eea026209c4450f2135d342a6bc057d9f4965a12
Rene A Larson <122948946+ReneLarson1990@users.noreply.github.com>
2023-10-22 19:00:00 -0700

    added favicon and adjusted index.html

--- ecommerce_backend/ecommerce_backend/views.py
+++ ecommerce_backend/ecommerce_backend/views.py
@@ -1,4 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+from ecommerce_backend.models import Product, Category, Vendor, CartOrder, CartOrderItems, ProductImages, ProductReviews, Wishlist, Address
+
 def index(request):
-    return render(request, 'index.html')
+    products = Product.objects.all()
+
+    context = {
+        "products": products
+    }
+
+    return render(request, 'index.html', context)
+
