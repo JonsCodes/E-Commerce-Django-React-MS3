c2c88cae67431d751794a30e27095f47fbbe7e89
“JonsCodes” <johnalyashai@me.com>
2023-10-23 20:33:29 -0700

    Working on user

--- api/user/models.py
+++ api/user/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    name = models.CharField(max_length=50, default='Anonymous')
+    email = models.EmailField(max_length=254, unique=True)
+
+    username = None
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    
