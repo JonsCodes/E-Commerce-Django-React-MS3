0a9351bddc68bc7cce683a024f41d5e241ebbfc3
“JonsCodes” <johnalyashai@me.com>
2023-10-23 20:12:54 -0700

    Completed products setup

--- api/product/models.py
+++ api/product/models.py
@@ -4,10 +4,15 @@ from api.category.models import Category
 # Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='products/', null=True, blank=True)
+    description = models.CharField(max_length=250)
+    price = models.CharField(max_length=50)
+    stock = models.CharField(max_length=50)
+    is_active = models.BooleanField(default=True, blank=True)
+    image = models.ImageField(upload_to='images/', null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+       return self.name
+    
--- api/urls.py
+++ api/urls.py
@@ -5,4 +5,6 @@ from .views import home
 urlpatterns = [
     path('', home, name='api.home'),
     path('category/', include('api.category.urls')),
+    path('product/', include('api.product.urls')),
+
 ]
