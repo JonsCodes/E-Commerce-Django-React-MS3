7790c04d58a5b334e6b1c1149bc4b59d13dca13a
“JonsCodes” <johnalyashai@me.com>
2023-10-23 19:45:45 -0700

    adding product serializers/urls and editing models

--- api/product/models.py
+++ api/product/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from api.category.models import Category
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='products/', null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
