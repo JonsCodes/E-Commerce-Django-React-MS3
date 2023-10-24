7e39d1c6fdf9b32506b91fdb60954a09c178b397
“JonsCodes” <johnalyashai@me.com>
2023-10-23 20:36:28 -0700

    Completed user setup

--- api/user/models.py
+++ api/user/models.py
@@ -12,4 +12,11 @@ class CustomUser(AbstractUser):
     REQUIRED_FIELDS = []
 
     phone = models.CharField(max_length=20, blank=True, null=True)
+    gender = models.CharField(max_length=10, blank=True, null=True)
+
+    session_token = models.CharField(max_length=10, default=0)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
     
