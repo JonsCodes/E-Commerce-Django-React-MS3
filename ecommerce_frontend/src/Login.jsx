52f9d23c0351e926900aa9d522f5a2686abe85a0
Benji250 <123209910+Benji250@users.noreply.github.com>
2023-10-25 17:47:21 -0700

    redid the register and login

--- ecommerce_frontend/src/Login.jsx
+++ ecommerce_frontend/src/Login.jsx
@@ -1,32 +1,112 @@
-import { useState } from "react"
+import { faDiagramSuccessor } from "@fortawesome/free-solid-svg-icons"
+import { useState, useRef, useEffect, useContext } from "react"
+import AuthContext from './context/AuthProvider'
+
+import axios from './api/axios'
+import { parseJsonSourceFileConfigFileContent } from "typescript"
+const LOGIN_URL = '/auth'
 
 
 
 
 
-const Login = (props) => {
-   
-  
-    const [email, setEmail] = useState('')
-    const [pass, setPass] = useState('')
 
-    const handleSubmit = (e) => {
+const Login = () => {
+    const { setAuth } = useContext(AuthContext)
+    const userRef = useRef()
+    const errRef = useRef()
+
+    const [user, setUser] = useState('')
+    const [pwd, setPwd] = useState('')
+    const [errMsg, setErrMsg] = useState('')
+    cosnt [success, setSuccess] = useState(false)
+
+    useEffect(() => {
+        userRef.current.focus()
+    }, [])
+
+    useEffect(() => {
+        setErrMsg('')
+    }, [user, pwd])
+
+    const handleSubmit = async (e) => {
         e.preventDefualt()
-        console.log(email)
+        
+        try{
+            const response = await axios.post(LOGIN_URL,
+                JSON.stringify({user, pwd}),
+                {
+                    headers: { 'Content-Type': 'application/json'},
+                    withCredentials: true
+                }
+            )
+            console.log(JSON.stringify(response?.data))
+            const accessToken = response?.data?.accessToken
+            const roles = response?.data?.roles
+            setAuth({ user, pwd, roles, accessToken })
+            setUser('')
+            setPwd('')
+            setSuccess(true)
+
+        }catch (err){
+            if (!err.response) {
+                setErrMsg("No Server Response")
+            } else if (err.response?.status === 400) {
+                setErrMsg('Missing Username pr Password')
+            } else if (err.response?.status === 401) {
+                setErrMsg('Unauthorized')
+            } else {
+                setErrMsg('Login Failed')
+            }
+            errRef.current.focus()
+        }
     }
 
     return (
-        <div className="auth-form-container">
-       
-            <form clasName="login-form" onSubmit={handleSubmit}>
-                    <label htmlfor="email">email</label>
-                    <input value={email} onChange={(e) => setEmail(e.target.vaule)} type="email" placeholder="youremail@gmail.com" id="email" name="email" />
-                    <label htmlfor="password">Password</label>
-                    <input value={pass} onChange={(e) => setPass(e.target.value)}type="password" placeholder="*******" id="passwoed" name="password" />
-                    <button type="submit">Log In</button>
-            </form>
-            <button onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here.</button>
-       </div>
+        <>
+            {success ? (
+                <section>
+                    <h1>You are logged in!</h1>
+                    <br />
+                    <p>
+                        <a href="#">Go to Home</a>
+                    </p>
+                </section>
+            ) : (
+       <section>
+        <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
+        <h1>Sign In</h1>
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="username">Username</label>
+            <input
+                type="text"
+                id="username"
+                ref={userRef}
+                autoComplete="off"
+                onChange={(e) => setUser(e.target.value)}
+                value={user}
+                required
+                />
+
+            <label htmlFor="password">Password</label>
+            <input
+                type="password"
+                id="password"
+                onChange={(e) => setPwd(e.target.value)}
+                value={pwd}
+                required
+                />
+                <button>Sign In</button>
+        </form>
+        <p>
+            Need an Account?<br />
+            <span className="line">
+                {/* Put router link here */}
+                <a href="#"></a>
+            </span>
+        </p>
+       </section>
+       )}</>
     )
 }
 export default Login
--- ecommerce_frontend/src/Register.jsx
+++ ecommerce_frontend/src/Register.jsx
@@ -1,30 +1,166 @@
-import { useState } from "react"
+import { useState, useRef, useEffect } from "react"
+import './styles/register.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+
+
+const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_] {3,23}$/;
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?-.*[0-9])(?=.*[!@#$%]).{8,24}$/
+
+const Register = () => {
+    const useRef = useRef()
+    const errRef = useRef()
+
+    const [user, setUser] = useState('')
+    const [validName, setValidName] = useState(false)
+    const [userFocus, setUserFocus] = useState(false)
+
+    const [pwd, setPwd] = useState('')
+    const [validPwd, setValidPwd] = useState(false)
+    const [pwdFocus, setPwdFocus] = useState(false)
+
+    const [matchPwd, setMatchPwd] = useState('')
+    const [validMatch, setValidMatch] = useState(false)
+    const [matchFocus, setMatchFocus] = useState(false)
+
+    const [errMsg, setErrMsg] = useState('')
+    const [success, setSuccess] = useState(false)
+
+    useEffect(() => {
+        userRef.current.focu()
+    }, []) 
+
+    useEffect(() => {
+        const result = USER_REGEX.test(user)
+        console.log(result)
+        console.log(user)
+        setValidName(result)
+    }, [user])
+
+    useEffect(() => {
+        const result = PWD_REGEX.test(pwd)
+        console.log(result)
+        console.log(pwd)
+        setValidPwd(result)
+        const match = pwd === matchPwd
+        setValidMatch(match)
+    }, [pwd, matchPwd])
+
+    useEffect(() => {
+        setErrMsg('')
+    }, [user, pwd, matchPwd])
 
 
 
-const Register = (props) => {
-    const [email, setEmail] = useState('')
-    const [pass, setPass] = useState('')
-    const [name, setNanme] = useState('')
-
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefualt()
-        console.log(email)
+        const v1 = USER_REGEX.test(user)
+        const v2 = PWD_REGEX.test(pwd)
+        if (!v1 || !v2) {
+            setErrMsg("Invalid Entry")
+            return
+        }
+
     }
 
     return (
-        <div className="auth-form-container">
+        <section>
+            <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
+            <h1>SignUp</h1>
             <form onSubmit={handleSubmit}>
-                    <label htmlFor="name">Fullname</label>
-                    <input value={name} name="name" id="name" placeholder="full name"></input>
-                    <label htmlfor="email">email</label>
-                    <input value={email} onChange={(e) => setEmail(e.target.vaule)} type="email" placeholder="youremail@gmail.com" id="email" name="email" />
-                    <label htmlfor="password">Password</label>
-                    <input value={pass} onChange={(e) => setPass(e.target.value)}type="password" placeholder="*******" id="passwoed" name="password" />
-                    <button type="submit">Log In</button>
+                <label htmlfor="username">
+                    Username:
+                    <span className={validName ? "valid" : "hide"}>
+                        <FontAwesomeIcon icon={faCheck} />
+                    </span>
+                    <span className={validName || !user ? "hide" : "invalid"}>
+                        <FontAwesomeIcon icon={faTimes} />
+                    </span>
+                </label>
+                <input
+                    type="text"
+                    id="username"
+                    ref={useRef}
+                    autoComplete="off"
+                    onChange={(e) => setUser(e.target.value)}
+                    required
+                    aria-invalid={validName ? "false" : "true"}
+                    aria-describedby="uidnote"
+                    onFocus={() => setUserFocus(true)}
+                    onBlur={() => setUserFocus(false)}
+                />
+                <p id="uidnote" className={userFocus && user && !validName ? "instructions" : "offscreen"}>
+                    <FontAwesomeIcon icon={faInfoCircle} />
+                    4 to 24 characters.<br />
+                    Must begin with a letter.<br />
+                    Letters, numbers, underscores, hyphens are allowed.
+                </p>
+                
+                <label htmlfor="password">
+                    Password:
+                    <span className={validName ? "valid" : "hide"}>
+                        <FontAwesomeIcon icon={faCheck} />
+                    </span>
+                    <span className={validPwd || !pwd ? "hide" : "invalid"}>
+                        <FontAwesomeIcon icon={faTimes} />
+                    </span>
+                </label>
+                <input
+                    type="password"
+                    id="password"
+                    onChange={(e) => setPwd(e.target.value)}
+                    required
+                    aria-invalid={validPwd ? "false" : "true"}
+                    aria-describedby="pwdnote"
+                    onFocus={() => setPwdFocus(true)}
+                    onBlur={() => setPwdFocus(false)}
+                />
+                <p id="pwdnote" className={pwdFocus && !validPwd ? "instructions" : "offscreen"}>
+                    <FontAwesomeIcon icon={faInfoCircle} />
+                    8 to 24 characters.<br />
+                    Must include uppercase and lowercase letters, a number and a special character.<br />
+                    Allowed special characters : <span aria-label="exclamation mark">!</span> <span aria-label="at symbol">@</span>
+                    <span aria-label="hashtag">#</span> <span aria-label="dollar sign">$</span>
+                    <span aria-label="percent">%</span>
+                </p>
+
+                <label htmlfor="confirm_pwd">
+                    Confirm Password:
+                    <span className={validMatch && matchPwd ? "valid" : "hide"}>
+                        <FontAwesomeIcon icon={faCheck} />
+                    </span>
+                    <span className={validMatch|| !matchPwd ? "hide" : "invalid"}>
+                        <FontAwesomeIcon icon={faTimes} />
+                    </span>
+                </label>
+                <input
+                    type="password"
+                    id="confirm_pwd"
+                    onChange={(e) => setMatchPwd(e.target.value)}
+                    required
+                    aria-invalid={validMatch ? "false" : "true"}
+                    aria-describedby="confirmnote"
+                    onFocus={() => setMatchFocus(true)}
+                    onBlur={() => setMatchFocus(false)}
+                />
+                <p id="pwdnote" className={matchFocus && !validMatch ? "instructions" : "offscreen"}>
+                    <FontAwesomeIcon icon={faInfoCircle} />
+                    Must match the first password inpit field.
+                </p>
+
+                <button disabled={!validName || !validPwd || !validMatch ? true : false}>
+                    Sign Up
+                </button>
             </form>
-            <button onClick={() => props.onFormSwitch('login')}>Already have an account? Login here.</button>
-       </div>
+            <p>
+                Already registered?<br />
+                <span className="line">
+                     {/* Put router link here */}
+                    <a href="#">Sign in</a>
+
+                </span>
+            </p>
+       </section>
     )
 }
 
