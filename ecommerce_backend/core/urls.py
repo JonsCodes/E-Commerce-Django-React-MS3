b468bbac41244fe110783af223d8ee6e0eda4eb6
Benji250 <123209910+Benji250@users.noreply.github.com>
2023-10-26 21:11:03 -0700

    added the backend and the home style

--- ecommerce_backend/core/urls.py
+++ ecommerce_backend/core/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for ecommerce_backend project.
+URL configuration for core project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/', include('api.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
